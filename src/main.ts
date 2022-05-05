2616da01361965a13e13b10b3a167268a3fc0189
Nils Ponsard <nilsponsard@gmail.com>
2022-05-05 11:42:29 +0200

    feat(api): add cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ import { getDataSource } from './data-source';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Polycode API')
