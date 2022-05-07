36030d57713b53f15e44777641d2ce39da61c150
Nils Ponsard <nilsponsard@gmail.com>
2022-05-07 14:11:48 +0200

    feat: add email validation

--- src/mail/dto/validate-mail.dto.ts
+++ src/mail/dto/validate-mail.dto.ts
@@ -1,6 +1,8 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty } from 'class-validator';
 
 export class ValidateMailDto {
+  @IsNotEmpty()
   @ApiProperty()
   code: string;
 }
