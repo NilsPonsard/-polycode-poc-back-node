3f978fc701764b681de124cc6303cfe854dba24d
Nils Ponsard <nilsponsard@gmail.com>
2022-05-10 18:04:44 +0200

    fix: fix crash on jwt expired

--- src/auth/check-user.ts
+++ src/auth/check-user.ts
@@ -5,28 +5,33 @@ export async function CheckUser(request: any): Promise<boolean> {
   const authorization = request.headers?.authorization as undefined | string;
 
   if (!authorization) return false;
-  const content = await verify(authorization.replace('Bearer ', ''));
+  try {
+    const content = await verify(authorization.replace('Bearer ', ''));
 
-  const accessToken = typeof content != 'string' ? content?.token : undefined;
-  if (!accessToken) return false;
+    const accessToken = typeof content != 'string' ? content?.token : undefined;
+    if (!accessToken) return false;
 
-  const queryResult = await Token.findOne({
-    where: { accessToken },
-    relations: { user: true },
-  });
+    const queryResult = await Token.findOne({
+      where: { accessToken },
+      relations: { user: true },
+    });
 
-  if (!queryResult) return false;
+    if (!queryResult) return false;
 
-  const { user, createdAt } = queryResult;
+    const { user, createdAt } = queryResult;
 
-  if (createdAt.getTime() + accessExpiration * 1000 < Date.now()) {
-    // delete old accessToken
-    accessToken.delete({ accessToken });
+    if (createdAt.getTime() + accessExpiration * 1000 < Date.now()) {
+      // delete old accessToken
+      accessToken.delete({ accessToken });
+      return false;
+    }
+    if (!user) return false;
+
+    request.user = user;
+    request.accessToken = accessToken;
+    return true;
+  } catch (err) {
+    console.log(err);
     return false;
   }
-  if (!user) return false;
-
-  request.user = user;
-  request.accessToken = accessToken;
-  return true;
 }
